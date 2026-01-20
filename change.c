aa256b4574748203194c124e4d09046a979d4de9
mhlohh <138890916+mhlohh@users.noreply.github.com>
2026-01-20 15:12:09 +0530

    Update change.c

--- change.c
+++ change.c
@@ -1,60 +1,29 @@
-#include <stdio.h>
-#include <string.h>
-const int quarters = 25;
-const int dimes = 10;
-const int nickels = 5;
-const int pennies = 1;
-const int MAX = 4; // total types of  coins
-const int coin[MAX] = {quarters, dimes, nickels, pennies};
-int coin_count(int coin_type, int change);
-struct transaction
-{
-    int money;
-    int cost;
-};
+#include<stdio.h>
 
+const int coins [] = {25,10,5,1};
 int main(void)
 {
-    struct transaction bill;
-    // prompting user for cost and money paid
-    printf("Cost: ");
-    scanf("%i", &bill.cost);
-    printf("Paid Amount ");
-    scanf("%i", &bill.money);
-    // Check weather the value is valid
-    if (bill.cost < 1 || bill.money < 1)
+    int MAX = 4;
+    int cost = 0;
+    int money = 0;
+    printf("Enter the cost: ");
+    scanf("%i",&cost);
+    printf("Enter the money: ");
+    scanf("%i",&money);
+    if (cost > money)
     {
-        printf("Error\n");
+        printf("Insuficent Fund\n");
         return 1;
     }
-    // check weather paid money is enough
-    int change = bill.money - bill.cost;
-    if (change < 0)
+    int change = money - cost;
+    int no_coins[]= {0,0,0,0};
+    for(int i = 0; i < MAX;i++)
     {
-        printf("Not enough cash\n");
-        return 2;
+      no_coins [i] = change/coins[i];
+      change = change%coins[i];
     }
-    printf("Change: %i\n",change);
-    // intilising the count array
-    int count_coin[MAX];
-    // count number of quaters
-    for (int i = 0; i < MAX; i++)
-    {
-        count_coin[i] = coin_count(coin[i], change);
-        change -= coin[i] * count_coin[i];
-    }
-    printf("Quarters: %i\nDimes: %i\nNickels: %i\nPennies %i\n", count_coin[0], count_coin[1],
-           count_coin[2], count_coin[3]);
-    return 0;
-}
-// function for counting coin
-int coin_count(int coin_type, int change)
-{
-    int count = 0;
-    while (change >= coin_type)
-    {
-        change -= coin_type;
-        count++;
-    }
-    return count;
+    printf("Quarter: %i\n",no_coins[0]);
+    printf("Dimes: %i\n",no_coins[1]);
+    printf("Nickels: %i\n",no_coins[2]);
+    printf("Pennies: %i\n",no_coins[3]);
 }
